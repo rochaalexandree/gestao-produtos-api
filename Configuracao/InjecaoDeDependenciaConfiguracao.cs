656077c8f1a74d383fb94a9a6eb70c38e9d9e143
Alexandre Rocha <rochaalexandree@gmail.com>
2024-02-18 20:39:25 -0300

    Configuracoes da startup

---
+++ Configuracao/InjecaoDeDependenciaConfiguracao.cs
@@ -0,0 +1,20 @@
+﻿using GestaoProdutos.Infraestrutura.Contextos;
+using GestaoProdutos.Infraestrutura.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestaoProdutos.Configuracao
+{
+    public static class InjecaoDeDependenciaConfiguracao
+    {
+        public static void RegistrarServicos(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+            //services.AddScoped<IFornecedorRepositorio, FornecedorRepositorio>();
+            services.AddScoped<AppContext>();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,51 +1,38 @@
+using GestaoProdutos.Configuracao;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GestaoProdutos
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public Startup(IHostEnvironment hostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddApiConfiguracao(Configuration);
+            services.AddSwaggerConfiguracao();
+            services.RegistrarServicos();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSwaggerConfiguration();
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
         }
     }
 }
