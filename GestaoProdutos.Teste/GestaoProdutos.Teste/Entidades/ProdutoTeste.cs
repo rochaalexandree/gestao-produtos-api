19263e69a2f8ee3d7248d977ab8fda53631bd5c8
Alexandre Rocha <rochaalexandree@gmail.com>
2024-02-20 00:46:53 -0300

    Testes da entidade Produto

--- GestaoProdutos.Teste/GestaoProdutos.Teste/Entidades/ProdutoTeste.cs
+++ GestaoProdutos.Teste/GestaoProdutos.Teste/Entidades/ProdutoTeste.cs
@@ -1,7 +1,8 @@
 using GestaoProdutos.Dominio.Entidades;
 using System;
 using Xunit;
-
+using FluentAssertions;
+using System.Linq;
 namespace GestaoProdutos.Teste
 {
     public class ProdutoTeste
@@ -11,7 +12,32 @@ namespace GestaoProdutos.Teste
         {
             var produto = new Produto("Produto teste", true, DateTime.Now, DateTime.Now.AddMonths(1), Guid.NewGuid());
 
+            produto.Should().NotBeNull();
+            produto.IsValid.Should().BeTrue();
+        }
 
+        [Fact(DisplayName = "Instanciar novo produto invalido com notification da data de fabricacao")]
+        public void Produto_InstaciarNovoProduto_DeveEstarInvalidoComNotificationDeDataDeFabricacao()
+        {
+            var produto = new Produto("Produto teste", true, DateTime.Now.AddMinutes(1), DateTime.Now, Guid.NewGuid());
+
+            produto.Should().NotBeNull();
+            produto.IsValid.Should().BeFalse();
+            produto.Notifications.Should().NotBeEmpty();
+            produto.Notifications.First().Message.Should().Be("A data de fabricação não pode ser maior ou igual a data de validade.");
+        }
+
+        [Fact(DisplayName = "Instanciar novo produto valido")]
+        public void Produto_AlterarDataDeValidade_DeveEstarInvalidoComNotificationDeDataDeValidade()
+        {
+            var produto = new Produto("Produto teste", true, DateTime.Now, DateTime.Now.AddMonths(1), Guid.NewGuid());
+            
+            produto.AlterarDataValidade(DateTime.Now.AddMonths(-2));
+            
+            produto.Should().NotBeNull();
+            produto.IsValid.Should().BeFalse();
+            produto.Notifications.Should().NotBeEmpty();
+            produto.Notifications.First().Message.Should().Be("A data de fabricação não pode ser maior ou igual a data de validade.");
         }
     }
 }
